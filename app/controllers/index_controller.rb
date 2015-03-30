88989348985aa459dbffc877cdd91b26a3492036
--replace-all <krystallattice@gmail.com>
2015-03-30 01:57:42 +0000

    styled

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -7,9 +7,11 @@ end
 
 get '/anagrams/:word' do
     @word = params[:word]
+    @anagrams = Word.find_anagrams(@word)
     erb :show
 end
 
 post '/' do
-    
+    @word = params[:word]
+    redirect "/anagrams/#{@word}"
 end
